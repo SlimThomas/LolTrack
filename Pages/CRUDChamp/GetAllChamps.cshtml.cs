ca8a0fcff42e8a4023807380fc06391bbeb6342a
MurtSkytte <mart175b@edu.zealand.dk>
2024-04-22 13:15:17 +0200

    GetAllRazorPage

--- Pages/CRUDChamp/GetAllChamps.cshtml.cs
+++ Pages/CRUDChamp/GetAllChamps.cshtml.cs
@@ -1,3 +1,5 @@
+using LolTrack.Models;
+using LolTrack.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,36 @@ namespace LolTrack.Pages.CRUDChamp
 {
     public class GetAllChampsModel : PageModel
     {
+        private ChampService _champservice;
+
+		public GetAllChampsModel(ChampService champservice)
+		{
+			_champservice = champservice;
+		}
+
+        public List<Champion> Champions { get; private set; }
+        [BindProperty]
+        public string Search { get; set; }
+
         public void OnGet()
         {
+            Champions = _champservice.GetChampions();
+        }
+
+        public IActionResult OnGetSortByName()
+        {
+            Champions = _champservice.SortByName().ToList();
+            return Page();
+        }
+        public IActionResult OnGetSortByNameDesc()
+        {
+            Champions = _champservice.SortByNameDesc().ToList();
+            return Page();
+        }
+        public IActionResult OnPostChampSearch()
+        {
+            Champions = _champservice.ChampSearch(Search).ToList();
+            return Page();
         }
     }
 }
