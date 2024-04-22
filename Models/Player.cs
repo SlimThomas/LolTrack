96164f415c1f0a7e79041a0a367da3c2a314fb80
MurtSkytte <mart175b@edu.zealand.dk>
2024-04-22 10:50:03 +0200

    Rune and player model

--- Models/Player.cs
+++ Models/Player.cs
@@ -2,5 +2,32 @@
 {
 	public class Player
 	{
+        public int PlayerID { get; set; }
+        public int TotalWins { get; set; }
+        public double WinPerC { get; set; }
+        public double KDA { get; set; }
+        public int TotalMatch { get; set; }
+        public int TotalLosses { get; set; }
+        public double AvgVision { get; set; }
+        public Match MostPlayedChamp { get; set; }
+        public Match MostPlayedLane { get; set; }
+
+		public Player(int playerID, int totalWins, double winPerC, double kDA, int totalMatch, 
+			int totalLosses, double avgVision, Match mostPlayedChamp, Match mostPlayedLane)
+		{
+			PlayerID = playerID;
+			TotalWins = totalWins;
+			WinPerC = winPerC;
+			KDA = kDA;
+			TotalMatch = totalMatch;
+			TotalLosses = totalLosses;
+			AvgVision = avgVision;
+			MostPlayedChamp = mostPlayedChamp;
+			MostPlayedLane = mostPlayedLane;
+		}
+
+		public Player()
+		{
+		}
 	}
 }
---
+++ Models/Rune.cs
@@ -0,0 +1,22 @@
+﻿namespace LolTrack.Models
+{
+	public class Rune
+	{
+        public int RuneID { get; set; }
+        public string RunePageName  { get; set; }
+        public Enum RuneType { get; set; }
+        public string RuneDesc  { get; set; }
+
+		public Rune(int runeID, string runePageName, Enum runeType, string runeDesc)
+		{
+			RuneID = runeID;
+			RunePageName = runePageName;
+			RuneType = runeType;
+			RuneDesc = runeDesc;
+		}
+
+		public Rune()
+		{
+		}
+	}
+}
