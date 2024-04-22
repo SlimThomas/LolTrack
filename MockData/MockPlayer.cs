66a86ba1156b35ff9ed57f2586bf3daef0f3ceed
MurtSkytte <mart175b@edu.zealand.dk>
2024-04-22 11:00:43 +0200

    MockPlayer

--- MockData/MockPlayer.cs
+++ MockData/MockPlayer.cs
@@ -1,6 +1,13 @@
-﻿namespace LolTrack.MockData
+﻿using LolTrack.Models;
+
+namespace LolTrack.MockData
 {
 	public class MockPlayer
 	{
+		private static List<Player> _players = new List<Player>();
+		public static List<Player> GetMockPlayer()
+		{
+			return _players;
+		}
 	}
 }
--- Services/PlayerService.cs
+++ Services/PlayerService.cs
@@ -1,6 +1,20 @@
-﻿namespace LolTrack.Services
+﻿using LolTrack.MockData;
+using LolTrack.Models;
+
+namespace LolTrack.Services
 {
 	public class PlayerService
 	{
+		private List<Player> _players;
+		
+		public PlayerService()
+		{
+			_players = MockPlayer.GetMockPlayer();
+		}
+
+		public void AddPlayer()
+		{
+			_players.Add(player);
+		}
 	}
 }
