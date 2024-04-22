14d4313cd52d31137b249e6f654e872cbc9ccaa2
MurtSkytte <mart175b@edu.zealand.dk>
2024-04-22 10:36:53 +0200

    Champion model klasse

--- Models/Champion.cs
+++ Models/Champion.cs
@@ -2,7 +2,23 @@
 {
 	public class Champion
 	{
-        public int CHampionID { get; set; }
+		public Champion()
+		{
+		}
+
+		public int ChampionID { get; set; }
         public string Name { get; set; }
-    }
+        public string Ability { get; set; }
+        public string Desc { get; set; }
+        public Enum Difficulty { get; set; }
+
+		public Champion(int championID, string name, string ability, string desc, Enum difficulty)
+		{
+			ChampionID = championID;
+			Name = name;
+			Ability = ability;
+			Desc = desc;
+			Difficulty = difficulty;
+		}
+	}
 }
