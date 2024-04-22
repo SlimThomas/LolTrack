0c37c3eb6ffe59557c8e0f836c1d288b158a3c2f
SlimThomas <147144450+SlimThomas@users.noreply.github.com>
2024-04-22 10:58:05 +0200

    Lavet model til User

---
+++ Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LolTrack.Models
+{
+    public class User
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserID { get; set; }
+
+        [Required(ErrorMessage = "Username må maks være på 20 tegn"), StringLength(20)]
+        public string UserName { get; set; }
+        [Required]
+        public string Passwrod { get; set; }
+
+        public User(string userName, string passwrod)
+        {
+            
+            UserName = userName;
+            Passwrod = passwrod;
+        }
+
+        public User() 
+        {
+            
+        }
+
+    }
+}
