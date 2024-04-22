7600d8f9bfda450df037d2d895eab92a10968393
SlimThomas <147144450+SlimThomas@users.noreply.github.com>
2024-04-22 10:49:15 +0200

    Lavet SSpells

---
+++ Models/SSpells.cs
@@ -0,0 +1,17 @@
+﻿namespace LolTrack.Models
+{
+    public class SSpells
+    {
+
+        public int SSpellID { get; set; }
+        public Enum SpellType { get; set; }
+        public string SpellDesc { get; set; }
+
+        public SSpells(int sSpellID, Enum spellType, string spellDesc)
+        {
+            SSpellID = sSpellID;
+            SpellType = spellType;
+            SpellDesc = spellDesc;
+        }
+    }
+}
