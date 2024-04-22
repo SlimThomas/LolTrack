808dca972cb79015993777775245d33a4a001d8e
SlimThomas <147144450+SlimThomas@users.noreply.github.com>
2024-04-22 11:59:27 +0200

    Create UserService.cs

---
+++ Services/UserService.cs
@@ -0,0 +1,27 @@
+﻿using LolTrack.Models;
+using System.Runtime.CompilerServices;
+
+namespace LolTrack.Services
+{
+    public class UserService
+    {
+        public List<User> _users { get; set; } 
+
+        // Lav en konstruktor, når der er lavet Json eller DB
+
+        public void AddUser (User user)
+        {
+            _users.Add(user);
+        }
+
+        public User GetUserByUsername (string username)
+        {
+            foreach (var user in _users)
+            {
+                if (user.UserName == username) return user;
+            }
+            return null; 
+        }
+
+    }
+}
