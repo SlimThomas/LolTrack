aef27c448e33dfcdee8ae17923b15213fc77862f
SlimThomas <147144450+SlimThomas@users.noreply.github.com>
2024-04-22 11:41:55 +0200

    MockRunes

---
+++ MockData/MockRunes.cs
@@ -0,0 +1,15 @@
+﻿using LolTrack.Models;
+
+namespace LolTrack.MockData
+{
+    public class MockRunes
+    {
+       
+        private static List<Rune> _runes = new List<Rune>();
+        public static List<Rune> GetMockRunes()
+        {
+            return _runes; 
+        }
+
+    }
+}
