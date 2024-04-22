8cc5e673101ef2c640b695e44e9d598837e94978
SlimThomas <147144450+SlimThomas@users.noreply.github.com>
2024-04-22 11:03:01 +0200

    Create MockMatch.cs

---
+++ MockData/MockMatch.cs
@@ -0,0 +1,22 @@
+﻿using LolTrack.Models;
+using System.Runtime.CompilerServices;
+
+namespace LolTrack.MockData
+{
+    public class MockMatch
+    {
+        
+        private static List<Match> _matches = new List<Match>();
+
+        public static List<Match> GetMockMatches()
+        {
+            return _matches; 
+        }
+        
+        
+       
+
+
+
+    }
+}
