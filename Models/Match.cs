95b9cb9c9cda048105eebb7f12201ee4261c3a43
SlimThomas <147144450+SlimThomas@users.noreply.github.com>
2024-04-22 10:46:27 +0200

    Update Match.cs

--- Models/Match.cs
+++ Models/Match.cs
@@ -2,5 +2,37 @@
 {
     public class Match
     {
+        public int MatchID { get; set; }
+        public bool WinProcent { get; set; }
+        public int Kills { get; set; }
+        public int Deaths { get; set; }
+        public int Assists { get; set; }
+        public double KDA { get; set; }
+        public int Visionscore { get; set; }
+        public Enum LaneType { get; set; }
+        public Enum Items {  get; set; }
+        public Enum Champions { get; set; }
+        public Enum Runes { get; set; }
+        public Enum SSpells { get; set; } 
+        public int Minions { get; set; }
+
+        public Match(int matchID, bool winProcent, int kills, int deaths, int assists, double kDA, int visionscore, Enum laneType, Enum items, Enum champions, Enum runes, Enum sSpells, int minions)
+        {
+            MatchID = matchID;
+            WinProcent = winProcent;
+            Kills = kills;
+            Deaths = deaths;
+            Assists = assists;
+            KDA = kDA;
+            Visionscore = visionscore;
+            LaneType = laneType;
+            Items = items;
+            Champions = champions;
+            Runes = runes;
+            SSpells = sSpells;
+            Minions = minions;
+        }
+
+        
     }
 }
