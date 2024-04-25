016c95fd797eef76a26c8d8a8e3683d7c7aaa4fb
MurtSkytte <mart175b@edu.zealand.dk>
2024-04-25 10:19:36 +0200

    create

--- MockData/MockChamp.cs
+++ MockData/MockChamp.cs
@@ -4,7 +4,12 @@ namespace LolTrack.MockData
 {
 	public class MockChamp
 	{
-		private static List<Champion> _champions = new List<Champion>();
+		private static List<Champion> _champions = new List<Champion>()
+		{
+			new Champion(1, "Aatrox", "He hits with q and shiz", "Aatrox is a big red demon guy and he angy", "Medium Difficulty"),
+			new Champion(2, "Annie", "Her teddybear becomes big and hits you", "Annie is a little girl who is cursed, her best friend is her teddybear", "Easy Difficulty"),
+			new Champion(159, "K'sante", "he hits you with q and shiz", "K'sante has big rocks for weapons", "Medium Difficulty")
+		};
 		public static List<Champion> GetMockChamps()
 		{
 			return _champions;
