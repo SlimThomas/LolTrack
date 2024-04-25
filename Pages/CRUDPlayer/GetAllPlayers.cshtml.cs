3bb6cf70e733eee73f1b26697cef6c1aa19bd1ac
SlimThomas <147144450+SlimThomas@users.noreply.github.com>
2024-04-25 12:04:29 +0200

    Lavet Delete og GetAllPlayers

--- Pages/CRUDPlayer/GetAllPlayers.cshtml.cs
+++ Pages/CRUDPlayer/GetAllPlayers.cshtml.cs
@@ -1,12 +1,22 @@
+using LolTrack.Models;
+using LolTrack.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net;
 
 namespace LolTrack.Pages.CRUDPlayer
 {
     public class GetAllPlayersModel : PageModel
     {
+        private PlayerService _playerService;
+        public List<Player> players {  get; set; }
+        public GetAllPlayersModel(PlayerService playerService)
+        {
+            this._playerService = playerService;
+        }
         public void OnGet()
         {
+            players = _playerService.GetPlayers(); 
         }
     }
 }
--- Pages/CRUDPlayer/RemovePlayer.cshtml.cs
+++ Pages/CRUDPlayer/RemovePlayer.cshtml.cs
@@ -1,3 +1,5 @@
+using LolTrack.Models;
+using LolTrack.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,30 @@ namespace LolTrack.Pages.CRUDPlayer
 {
     public class RemovePlayerModel : PageModel
     {
-        public void OnGet()
+        private PlayerService _playerService;
+        [BindProperty]
+        public Player player { get; set; }
+        public RemovePlayerModel(PlayerService playerService)
         {
+            _playerService = playerService;
+        }
+        public IActionResult OnGet(int id)
+        {
+            player = _playerService.GetPlayer(id);
+            if (player == null)
+            {
+                return RedirectToPage("/NotFound"); 
+            }
+            return Page(); 
+        }
+        public IActionResult OnPost() 
+        {
+            Player deletedPlayer = _playerService.DeletePlayer(player.PlayerID);
+            if (deletedPlayer == null)
+            {
+                return RedirectToPage("/NotFound"); 
+            }
+            return RedirectToPage("GetAllPlayers"); 
         }
     }
 }
