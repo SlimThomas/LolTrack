a7148ed349a19812a10596653329a1f83bf1bee3
SlimThomas <147144450+SlimThomas@users.noreply.github.com>
2024-05-14 14:03:40 +0200

    Lavet footer, samt SSPells page(dog ikke done endnu)

--- MockData/MockSSpell.cs
+++ MockData/MockSSpell.cs
@@ -7,15 +7,15 @@ namespace LolTrack.MockData
 
         private static List<SSpell> _SSpells = new List<SSpell>()
         {
-            new SSpell(1, SSpells.Flash, "placeholder", "Flash.png"),
-            new SSpell(2, SSpells.Ignite, "placeholder", "Ignite.png"),
-            new SSpell(3, SSpells.Teleport, "placeholder", "TP.png"),
-            new SSpell(4, SSpells.Heal, "placeholder", "Heal.png"),
-            new SSpell(5, SSpells.Ghost, "placeholder", "Ghost.png"),
-            new SSpell(6, SSpells.Barrier, "placeholder", "Barrier.png"),
-            new SSpell(7, SSpells.Exhaust, "placeholder","Exhaust.png"),
-            new SSpell(8, SSpells.Smite, "placeholder", "Smite.png"),
-            new SSpell(9, SSpells.Cleanse, "placeholder", "Cleanse.png")
+            new SSpell(1, SSpells.Flash, "placeholder", "Flash_HD.jpg"),
+            new SSpell(2, SSpells.Ignite, "placeholder", "Ignite_HD.jpg"),
+            new SSpell(3, SSpells.Teleport, "placeholder", "Teleport_HD.jpg"),
+            new SSpell(4, SSpells.Heal, "placeholder", "Heal_HD.jpg"),
+            new SSpell(5, SSpells.Ghost, "placeholder", "Ghost_HD.jpg"),
+            new SSpell(6, SSpells.Barrier, "placeholder", "Barrier_HD.jpg"),
+            new SSpell(7, SSpells.Exhaust, "placeholder","Exhaust_HD.jpg"),
+            new SSpell(8, SSpells.Smite, "placeholder", "Smite_HD.jpg")
+            
         };
 
         public static List<SSpell> GetMockSSpells()
